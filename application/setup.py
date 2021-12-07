d7c638ac6d131adb166dadd869c3223dd43c8c5f
Timur-Rustamov <84975945+Timur-Rustamov@users.noreply.github.com>
2021-12-07 12:52:45 +0300

    Update setup.py

--- application/setup.py
+++ application/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='demo',
-    version='0.6',
+    version='0.7',
     long_description=__doc__,
     packages=['demo'],
     include_package_data=False,
